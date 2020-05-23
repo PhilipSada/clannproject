b2c81ef12ab5d06b083fe6d62ae51d2e36c30a6c
Philip Sada <sadaphillip@gmail.com>
2020-05-23 22:04:54 +0200

    added more codes to product.php

--- app/Product.php
+++ app/Product.php
@@ -5,7 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
-{
+{   
+    protected $fillable = [
+        'title', 'productImage', 'description','priceRange','otherPriceRange','created_by_name','created_by_email','approve',
+        'category_id','category_title'
+    ];
     public function category(){
         $this->belongsTo('App\ProductCategory');
     }
